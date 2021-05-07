75ec74de1b541a5a71090a5c23cb1ec9f1cb4386
Phill Janowski <Phill.Janowski@nisc.coop>
2021-05-07 16:19:53 -0500

    wrote tests. Going head long into the logical expression oracle cuz i can't get the custom circuit to do what it should

--- phills_part.py
+++ phills_part.py
@@ -1,7 +1,7 @@
 import ast
 import math
 import random
-from typing import Tuple, Union
+from typing import Tuple, Union, List
 
 from numpy import pi
 from qiskit.aqua.algorithms import Grover
@@ -20,6 +20,7 @@ from qiskit.visualization import plot_histogram
 
 L = []
 y = 0
+N = 0
 
 
 def get_pi_perm(n):
@@ -49,16 +50,17 @@ def get_L(N):
 
 
 def run():
-    global L, y
+    global L, y, N
     N = 16
     L = get_L(N)
     y = random.choice(range(N))
-    m = 10
+    m = 20
     for i in range(m):
         print(f"y = {y} | {format(y, 'b').zfill(4)}")
         print(f"L[y] = {L[y]}")
         number_of_qibits = math.ceil(math.log(N, 2))
         qreg = QuantumRegister(number_of_qibits)
+        output = QuantumRegister(number_of_qibits)
         creg = ClassicalRegister(number_of_qibits)
         # qc = QuantumCircuit(register)
         # for i in range(number_of_qibits):
@@ -68,34 +70,56 @@ def run():
         # qc.cz(0, 3)
         # qc.h([0, 1, 2, 3])
 
-        qc = QuantumCircuit(qreg, name='qc')
-        for i in range(number_of_qibits):
-            qc.h(qreg[i])
+        result = grover_with_logical_expression()
 
-        # circuit_oracle = CustomCircuitOracle(variable_register=qreg, output_register=qreg, circuit=qc,
-        #                                      evaluate_classically_callback=f_L)
-        # grover = Grover(circuit_oracle)
-        expression = get_boolean_functions_from_truth_table_logical_oracle()
-        grover = Grover(oracle=LogicalExpressionOracle(expression=expression))
-        result = grover.run(QuantumInstance(BasicAer.get_backend('qasm_simulator'), shots=1024))
+        # result = grover_with_custom_circuit(number_of_qibits, output, qreg)
         # backend = BasicAer.get_backend('qasm_simulator')
         # results = execute(grover, backend, shots=1000).result()
         # counts = results.get_counts(grover)
         # print(result)
         measurements = result.get('measurement')
-        print(measurements)
+        # print(measurements)
+        get_formatted_measurements(measurements)
         plot_histogram(measurements)
 
         # amplified_state = get_amplified_state(counts)
-        y_primed = int("0b" + result.get('top_measurement'), 2)
+        y_primed = int("0b" + str(result.get('top_measurement')), 2)
         # print(f"amplified_state {amplified_state}")
         set_new_y(y_primed)
+
+    print(f"final result of grover search, smallest y = {y} L[y] = {L[y]}")
+    print(f"final result of y should be {L.index(0)}")
+
+
+def get_formatted_measurements(measurements: dict):
+    ret = {}
+    for measurement in measurements.items():
+        ret[str(int("0b" + str(measurement[0]), 2))] = measurement[1], measurement[0]
+    print(ret)
+
+def grover_with_custom_circuit(number_of_qibits, output, qreg):
+    qc = QuantumCircuit(qreg, output, name='qc')
+    for i in range(number_of_qibits):
+        qc.h(qreg[i])
+    circuit_oracle = CustomCircuitOracle(variable_register=qreg, output_register=qreg, circuit=qc,
+                                         evaluate_classically_callback=f_L)
+    grover = Grover(oracle=circuit_oracle)
+    result = grover.run(QuantumInstance(BasicAer.get_backend('qasm_simulator'), shots=1024))
+    return result
+
+
+def grover_with_logical_expression():
+    expression = get_boolean_functions_from_truth_table_logical_oracle()
+    grover = Grover(oracle=LogicalExpressionOracle(expression=expression, optimization=True))
+    return grover.run(QuantumInstance(BasicAer.get_backend('qasm_simulator'), shots=1024))
 
 
 def set_new_y(y_primed):
     global L, y
+    print(f"old y value {y}")
     if L[y_primed] < L[y]:
         y = y_primed
+    print(f"new y value {y}")
 
 
 def get_amplified_state(counts: Counts):
@@ -111,76 +135,76 @@ def draw(qc: QuantumCircuit):
 
 
 def get_boolean_functions_from_truth_table():
-    variables = ['{0}', '{1}', '{2}', '{3}']
+    global N
+    variables = [' {0} ', ' {1} ', ' {2} ', ' {3} ']
     table = get_truth_table()
     # table = '0000000000000001'
-    truth_string = ''
     OR = ' or '
     AND = ' and '
     NOT = ' not '
     clauses = []
+    truth_string = boolean_function_builder(AND, NOT, OR, clauses, table, variables)
+    return truth_string
+
+
+def boolean_function_builder(AND, NOT, OR, clauses, table, variables):
+    global N
     for i in range(len(table)):
-        cur_clause = ''
-        if table[i] == '0':
-            cur_clause += '('
-            B = format(i, 'b').zfill(4)
+        if table[i] == '1':
+            cur_clause = ' ( '
+            B = format(i, 'b').zfill(int(math.log2(N)))
             for j in range(len(B)):
                 if B[j] == '0':
                     cur_clause += NOT
                 cur_clause += variables[j]
                 if j < (len(B) - 1):
                     cur_clause += AND
-            cur_clause += ' )'
+            cur_clause += ' ) '
             clauses.append(cur_clause)
     # if there were no clauses generated that means the truth table returned all 0's
     # meaning that L[y] is already lowest so we just let all states through
     if len(clauses) == 0:
         clauses.append(OR.join(variables))
-    else:
-        truth_string = OR.join(clauses)
+    truth_string = OR.join(clauses).replace('  ', ' ')
     return truth_string
 
 
 def get_boolean_functions_from_truth_table_logical_oracle():
     variables = ['w', 'x', 'y', 'z']
     table = get_truth_table()
-    truth_string = ''
-    OR = ' ^ '
+    OR = ' | '
     AND = ' & '
     NOT = ' ~'
     clauses = []
-    for i in range(len(table)):
-        cur_clause = ''
-        if table[i] == '0':
-            cur_clause += '('
-            B = format(i, 'b').zfill(4)
-            for j in range(len(B)):
-                if B[j] == '0':
-                    cur_clause += NOT
-                cur_clause += variables[j]
-                if j < (len(B) - 1):
-                    cur_clause += AND
-            cur_clause += ' )'
-            clauses.append(cur_clause)
-    # if there were no clauses generated that means the truth table returned all 0's
-    # meaning that L[y] is already lowest so we just let all states through
-    if len(clauses) == 0:
-        clauses.append(AND.join(variables))
-
-    truth_string = OR.join(clauses)
+    truth_string = boolean_function_builder(AND, NOT, OR, clauses, table, variables)
     return truth_string
 
 
-def f_L(bits: str) -> Tuple[bool, list[int]]:
+def f_L(bits: str) -> Tuple[bool, List[int]]:
+    global L, y
     # just gonna assume that bits is 4 bytes long
-
     function = get_boolean_functions_from_truth_table()
-    function = function.replace('{0}', str(bool(int(bits[0]))))
-    function = function.replace('{1}', str(bool(int(bits[1]))))
-    function = function.replace('{2}', str(bool(int(bits[2]))))
-    function = function.replace('{3}', str(bool(int(bits[3]))))
-    ret = eval(function)
-    ret_list = [ret, ret ^ bool(bits[1]), ret ^ bool(bits[2]), ret ^ bool(bits[3])]
+    # replaced_function = function.replace('{3}', str(bool(int(bits[3]))))
+    # replaced_function = replaced_function.replace('{2}', str(bool(int(bits[2]))))
+    # replaced_function = replaced_function.replace('{1}', str(bool(int(bits[1]))))
+    # replaced_function = replaced_function.replace('{0}', str(bool(int(bits[0]))))
+    replaced_function = function
+    for i in range(len(bits)):
+        replaced_function = replaced_function.replace(f"{{{i}}}", str(bool(int(bits[i]))))
+    ret = eval(replaced_function)
+    try:
+        assert ret == (int(bits, 2) < L[y])
+    except Exception:
+        print(f"Failed on {int(bits, 2)} < {L[y]}")
+        print(f"bits looks like {bits}")
+        print(f"Expression looks like {replaced_function}")
+        function_parts = function.split('^')
+        for function_part in function_parts:
+            print(f"{function_part} evaluated to {eval(function_part)}")
+        raise AssertionError
+    ret_list = [ret]
+    for bit in bits:
+        ret_list.append(int(ret ^ bool(bit)))
     # ret_list = [8, 9, 4, 7]
     return ret, ret_list
 
@@ -191,11 +215,12 @@ def get_truth_table():
     L_y = L[y]
     for i in range(len(L)):
         if L[i] < L_y:
-            truth_table += '1'
+            truth_table += '0'
             print(f"L[{i}] is less than L[y]")
             print(f"L[{i}] = {L[i]}")
         else:
-            truth_table += '0'
+            truth_table += '1'
+    print(f"truth table = {truth_table}")
     return truth_table
 
 
--- test_phills_part.py
+++ test_phills_part.py
@@ -1,32 +1,35 @@
 import math
+import random
 from unittest import TestCase
 import phills_part
 
+
 class Test(TestCase):
-    def test_get_superposition_n(self):
-        v_0 = [
-            [1],
-            [0]
-        ]
+    def test_truth_table_generator(self):
+        phills_part.N = 16
+        phills_part.L = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        phills_part.y = 4
+        table = phills_part.get_truth_table()
+        expected = '1111000000000000'
+        self.assertEqual(expected, table)
 
-        v_1 = [
-            [0],
-            [1]
-        ]
+        phills_part.N = 4
+        phills_part.L = [1, 0, 3, 2]
+        phills_part.y = 2
+        expected = '1101'
+        table = phills_part.get_truth_table()
+        self.assertEqual(expected, table)
 
-        v_2 = [
-            [1],
-            [1]
-        ]
+    def test_boolean_function_getter(self):
+        phills_part.N = 4
+        phills_part.L = [1, 0, 3, 2]
+        phills_part.y = 2
 
-        v_5 = [
-            [0],
-            [1],
-            [1]
-        ]
-
-        self.assertEqual(v_0, phills_part.get_superposition(0, 1))
-        self.assertEqual(v_1, phills_part.get_superposition(1, 1))
-        self.assertEqual(v_2, phills_part.get_superposition(2, 2))
-        self.assertEqual(v_5, phills_part.get_superposition(5, math.ceil(math.log(5, 2))))
-
+        function = phills_part.get_boolean_functions_from_truth_table().strip()
+        # truth table will be 1101
+        # so for index 0 we will get back 00 so its specific function will be ( not {0} and not {1} )
+        # but we only want it returning true on the 1 states
+        expected = '''
+            ( not {0} and not {1} ) or ( not {0} and {1} ) or ( {0} and {1} )
+        '''.replace('\n', '').strip()
+        self.assertEqual(expected, function)
